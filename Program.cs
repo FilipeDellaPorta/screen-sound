9599dd30ab96da767c46f26e6f679ec69a7ec798
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-12-09 18:33:14 -0300

    chamando o método de exibir lista de artistas ordenados

--- Program.cs
+++ Program.cs
@@ -7,9 +7,11 @@ using (HttpClient client = new HttpClient())
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        //Console.WriteLine(resposta);
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
-        LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+
     }
     catch (Exception ex)
     {
