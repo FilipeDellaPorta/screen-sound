245a5b73e2af98b1bf8d2bfee82a4da32d5d7e50
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-12-09 18:17:31 -0300

    trabalhando com LINQ para filtrar as músicas por gênero

---
+++ Filtros/LinqFilter.cs
@@ -0,0 +1,15 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Filtros;
+
+internal class LinqFilter
+{
+    public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
+    {
+        var todosOsGenerosMusicais = musicas.Select(generos => generos.Genero).Distinct().ToList();
+        foreach (var genero in todosOsGenerosMusicais)
+        {
+            Console.WriteLine($"- {genero}");
+        }
+    }
+}
