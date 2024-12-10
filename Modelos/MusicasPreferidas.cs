745b5ad5805e9b5f3af5f6349cdb3c157cdcc6a8
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-12-10 19:00:18 -0300

    criando arquivos no C#

--- Modelos/MusicasPreferidas.cs
+++ Modelos/MusicasPreferidas.cs
@@ -1,4 +1,5 @@
-﻿namespace ScreenSound.Modelos;
+﻿using System.Text.Json;
+namespace ScreenSound.Modelos;
 
 internal class MusicasPreferidas
 {
@@ -24,5 +25,18 @@ internal class MusicasPreferidas
             Console.WriteLine($"- {musica.Nome} de {musica.Artista}");
         }
         Console.WriteLine();
+    }
+
+    public void GerarArquivoJson()
+    {
+        string json = JsonSerializer.Serialize(new
+        {
+            nome = Nome,
+            musicas = ListaDeMusicasFavoritas
+        });
+        string nomeDoArquivo = $"musicas-favoritas-{Nome}.json";
+
+        File.WriteAllText(nomeDoArquivo, json);
+        Console.WriteLine("O arquivo Json criado com sucesso!");
     }
 }
