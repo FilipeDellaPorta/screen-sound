50d912c1163facbeb98a8acda60b75134072a385
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-12-11 21:17:11 -0300

    adicionado a propriedade key

--- Modelos/Musica.cs
+++ Modelos/Musica.cs
@@ -5,13 +5,15 @@ namespace ScreenSound.Modelos;
 internal class Musica
 {
     [JsonPropertyName("song")]
-    public string Nome { get; set; }
+    public string? Nome { get; set; }
     [JsonPropertyName("artist")]
-    public string Artista { get; set; }
+    public string? Artista { get; set; }
     [JsonPropertyName("duration_ms")]
     public int Duracao { get; set; }
     [JsonPropertyName("genre")]
-    public string Genero { get; set; }
+    public string? Genero { get; set; }
+    [JsonPropertyName("key")]
+    public int Key { get; set; }
 
     public void ExibirDetalhesDaMusica()
     {
@@ -19,5 +21,6 @@ internal class Musica
         Console.WriteLine($"Música: {Nome}");
         Console.WriteLine($"Duração em segundos: {Duracao / 1000}");
         Console.WriteLine($"Gênero musical: {Genero}");
+        Console.WriteLine($"Tonalidade: {Key}");
     }
 }
